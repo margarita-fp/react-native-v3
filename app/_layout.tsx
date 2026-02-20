66a03cfd9d74d2c1e99bcd8c43a08d7a44b6d53e
Margarita Khodzka <mkhodzka@gmail.com>
2026-02-20 16:58:14 -0500

    bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-      <Stack.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulian }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="counter"
         options={{
           title: "Counter",
-          presentation: "modal",
-          animation: "slide_from_bottom",
+          tabBarIcon: ({ color, size }) => (
+            <EvilIcons name="clock" size={size} color={color} />
+          ),
         }}
       />
-      <Stack.Screen
+      <Tabs.Screen
         name="idea"
         options={{
           title: "Idea",
-          presentation: "modal",
-          animation: "slide_from_bottom",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="lightbulb-o" size={size} color={color} />
+          ),
         }}
       />
-    </Stack>
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,10 @@
 import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{ textAlign: "center", fontSize: 18, marginBottom: 24 }}
-      >
-        Go to Counter
-      </Link>
-
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted={true} />
       <ShoppingListItem name="Sugar" isCompleted />
