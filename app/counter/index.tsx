d4f3d159a98c617c8b3838a4179a1610582f9fd4
Margarita Khodzka <Margarita.FoppaPedretti@people.inc>
2026-04-07 10:41:06 -0400

    countdown timer

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -3,8 +3,18 @@ import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+import { useState, useEffect } from "react";
 
 export default function CounterScreen() {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((val) => val + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
     if (result === "granted") {
@@ -29,6 +39,7 @@ export default function CounterScreen() {
 
   return (
     <View style={styles.container}>
+      <Text>{secondsElapsed}</Text>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
